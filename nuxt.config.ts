6be9b82f3ef0b0acdba4efe1e887a0ab59ca4288
wasck <Waldemar.Schik@gmail.com>
2023-04-23 15:36:39 +0200

    chore: initialize project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
+export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   target: 'static',
-  router: {
-    base: '/wasck.github.io/'
-  },
   css: [
-    '@/public/css/font.css'
+    '@/public/css/main.css',
+    '@/public/css/scrollbar.css',
+    '@/public/css/glassmorphism.css'
   ],
   tailwindcss: {
     cssPath: '@/public/css/tailwind.css',
     configPath: 'tailwind.config',
     exposeConfig: false,
     exposeLevel: 2,
-    config: {},
     injectPosition: 'first',
     viewer: true,
   },
-}
+})
