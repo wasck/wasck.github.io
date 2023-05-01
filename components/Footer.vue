134b85b62aae0e56a639830d8d266c4c95d454af
wasck <Waldemar.Schik@gmail.com>
2023-05-01 19:04:31 +0200

    feat(wasck): add default layout

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="absolut bottom-0 fixed flex gap-2 h-4 p-3 w-screen items-center justify-around bg-glass ">
+    &copy; {{ fullName }} ({{ currentYear }}) </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const firstName = ref('Waldemar');
+const lastName = ref('Schik');
+const fullName = ref(`${firstName.value} ${lastName.value}`);
+const currentYear = ref(new Date().getFullYear());
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fixed flex gap-2 h-16 p-8 w-screen items-center justify-between drop-shadow-md bg-glass">
+    <h1 class="uppercase tracking-[4px] lg:font-bold">Waldemar Schik</h1>
+
+    <ol class="flex flex-row capitalize">
+      <a v-href="link.link" v-for="link in links">
+        <li class="hover:bg-[#45364B] hover:cursor-pointer rounded-r-md p-2">
+          {{ link.label }}
+        </li>
+      </a>
+    </ol>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const links = ref([
+  // { label: 'about me', link: '#' },
+  // { label: 'my life', link: '#' },
+]);
+</script>
